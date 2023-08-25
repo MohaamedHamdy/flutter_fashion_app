2fb1cc51592d8b97ae6b59ebcf4aaa74a9581720
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-08-25 22:11:04 +0300

    create splah screen body (not finished)

--- lib/features/splash/presentation/views/widgets/splash_screen_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_screen_body.dart
@@ -5,6 +5,27 @@ class SplashScreenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      height: double.maxFinite,
+      width: double.maxFinite,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(
+            'https://images.unsplash.com/photo-1623083158169-4a6c72813971?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
+          ),
+        ),
+      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.04,
+          ),
+          const Text(
+            'TheSociety',
+          ),
+        ],
+      ),
+    );
   }
 }
