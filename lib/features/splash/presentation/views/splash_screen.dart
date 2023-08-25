2180eb916d8ed0752d39f30814cd4cfd1724302c
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-08-26 00:40:40 +0300

    add app bar to splash screen

--- lib/features/splash/presentation/views/splash_screen.dart
+++ lib/features/splash/presentation/views/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fashion_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 import 'widgets/splash_screen_body.dart';
@@ -7,8 +8,19 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SplashScreenBody(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'TheSociety',
+          style: TextSytles.textStyle24.copyWith(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(175, 144, 122, 1),
+      ),
+      body: const SplashScreenBody(),
     );
   }
 }
